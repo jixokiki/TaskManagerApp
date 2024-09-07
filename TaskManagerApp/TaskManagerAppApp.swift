73563c14901d464bc6dea8580968b4d3f8fbcf66
Rizki Maulana <rrizki402@gmail.com>
2024-09-07 07:38:37 +0700

    Initial Commit

---
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by  Rizki Maulana on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
